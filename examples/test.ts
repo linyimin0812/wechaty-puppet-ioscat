eddf3c495eb82bc403d043380ad7a90a28dcc64e
lym152898 <linyimin520812@gmail.com>
2018-08-03 22:35:43 +0800

    as any to test

--- examples/test.ts
+++ examples/test.ts
@@ -4,14 +4,25 @@ import { PuppetIoscat } from '../src/'
 import { log } from '../src/config'
 
 const puppet = new PuppetIoscat({
-  token: 'xxx',
+  token: 'wxid_tdax1huk5hgs12',
 })
 
 const wechaty = new Wechaty({ puppet })
 
 wechaty.on('login', (user) => log.silly(`login: ${user}`))
-.on('message', (msg) => {
-  log.silly(`msg: ${msg}`)
-})
+  .on('message', async (msg) => {
+    const from = msg.from()
+    const to = msg.to()
+    const room = msg.room()
+    if (from) {
+      log.silly(from.name())
+    }
+    if (to) {
+      log.silly(to.name())
+    }
+    if (room) {
+      log.silly(await room.topic())
+    }
+  })
 
 wechaty.start()

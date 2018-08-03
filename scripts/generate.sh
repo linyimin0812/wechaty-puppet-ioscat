e9870663f4bef5a628ba67279a725d57cc6c1806
Huan LI <zixia@zixia.net>
2018-08-03 22:34:30 +0800

    use swager codegen v2.3.1 stable

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,7 +3,7 @@ set -e
 
 docker run --rm -v \
   ${PWD}:/local \
-  swaggerapi/swagger-codegen-cli \
+  swaggerapi/swagger-codegen-cli:v2.3.1 \
     generate \
     -i /local/swagger/ioscat.yaml \
     -l typescript-node \

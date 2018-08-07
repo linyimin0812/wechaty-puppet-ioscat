a7f731a2dd09d9906439642d29eef6f9818f730a
lym152898 <linyimin520812@gmail.com>
2018-08-07 15:24:46 +0800

    Change example

--- examples/test.ts
+++ examples/test.ts
@@ -4,12 +4,14 @@ import { PuppetIoscat } from '../src/'
 import { log } from '../src/config'
 
 const puppet = new PuppetIoscat({
-  token: 'xxxxxxxxxxxxx',
+  token: 'wxid_tdax1huk5hgs12',
 })
 
 const wechaty = new Wechaty({ puppet })
 
-wechaty.on('login', (user) => log.silly(`login: ${user}`))
+wechaty.on('login', async (user) => {
+  log.silly(`login: ${user}`)
+})
   .on('message', async (msg) => {
     const from = msg.from()
     const to = msg.to()
@@ -24,5 +26,28 @@ wechaty.on('login', (user) => log.silly(`login: ${user}`))
       log.silly('test', 'message: %s', await room.topic())
     }
   })
+  .on('error', (err) => {
+    log.error(JSON.stringify(err))
+  })
 
-wechaty.start()
+async function start () {
+  await wechaty.start()
+  const contact = await wechaty.Contact.find({ name: '林贻民' })
+  if (contact) {
+    // log.silly('发语音')
+    // contact.say('hello')
+  } else {
+    log.silly('null')
+  }
+
+  const room = await wechaty.Room.find({ topic: '直播一群' })
+  if (room) {
+    log.silly('room 发消息')
+    // room.say('hello')
+  } else {
+    log.silly('没有找到群')
+  }
+
+}
+
+start()
